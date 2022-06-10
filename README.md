e4c0694e36edb6a7a5e29ed0209e4641ad821ba3
Frolleks <spaceactive.akbar@gmail.com>
2022-06-10 20:49:39 +0700

    feat: add files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cinny (Electron)
+Cinny desktop app built with Electron. ~~*sorry I must use Electron because Tauri was a bit of a hell for me*~~
---
+++ src/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+      width: 1280,
+      height: 800
+    });  
+    mainWindow.loadFile(path.join(__dirname, '../cinny/dist/index.html'));
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+});
+
