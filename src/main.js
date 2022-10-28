8ef8f7651764a6dd049a6c8bc498f6227188c61e
Frolleks <frolleks@hotmail.com>
2022-10-28 20:50:57 +0700

    refactor: format code

--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-const createWindow = () => {
-    const mainWindow = new BrowserWindow({
-      backgroundColor: '#26292c',
-      width: 1280,
-      height: 800,
-    });  
-    mainWindow.loadFile(path.join(__dirname, '..', 'cinny', 'dist', 'index.html'));
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    backgroundColor: "#26292c",
+    width: 1280,
+    height: 800,
+  });
+  mainWindow.loadFile(
+    path.join(__dirname, "..", "cinny", "dist", "index.html")
+  );
 }
 
 app.whenReady().then(() => {
-    createWindow()
-    
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    });
+  createWindow();
+  
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
-
